0aaafe24098e8e400ad68cca4e28f1dd4ac04d81
cschneck <cora.schneck@gmail.com>
2018-02-06 22:58:43 -0700

    check docker is running, testing script within docker to ensure installed

--- 1_run_inside_repo.sh
+++ 1_run_inside_repo.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 echo "Copying files to docker container..."
 echo ""
+
+DOCKER_RUNNING=`docker ps -a | grep mcparseface | wc -l`
+
+if [ $DOCKER_RUNNING -eq "0" ]; then
+   echo "ERROR: docker is not running, start docker and then restart, exiting";
+   exit;
+fi
+
 docker cp ${PWD}/ mcparseface:/root/models/syntaxnet/Ishmael
 echo "done, copied, enter docker to run files"
--- 2_run_inside_docker_container.sh
+++ 2_run_inside_docker_container.sh
@@ -8,4 +8,7 @@ echo 'y' | apt-get install vim
 echo ""
 echo "install nltk for tokens"
 pip install nltk
-echo -e "import nltk\nnltk.download('punkt')" | python
+echo -e "import nltk\nnltk.download('punkt')\nnltk.download('averaged_perceptron_tagger')" | python
+
+# To be commented out (for testing)
+python raw_text_processing.py -F testing.txt 
