fa3a305de40a7c3e6b9b2bf8eeb8f3fbd93e65df
cschneck <cora.schneck@gmail.com>
2018-02-12 23:45:56 -0700

    save csv internally as namedtuple

--- raw_text_processing.py
+++ raw_text_processing.py
@@ -10,10 +10,16 @@ from nltk.tokenize import sent_tokenize, word_tokenize # form tokens from words/
 import string
 import csv
 from datetime import datetime
+from collections import namedtuple
+from itertools import imap # set up namedtuple
 ########################################################################
 ## READING AND TOKENIZATION OF RAW TEXT (PRE-PROCESSING)
 
 basic_pronouns = "I Me You She He Him It We Us They Them Myself Yourself Himself Herself Itself Themselves My your Her Its Our Their His"
+possessive_pronouns = "mine yours his hers ours theirs"
+reflexive_pronouns = "myself yourself himself herself itself oneself ourselves yourselves themselves"
+relative_pronouns = "that whic who whose whom where when"
+
 
 def readFile(filename):
 	file_remove_extra = []
@@ -93,6 +99,171 @@ def process_POS_conll(conll_output):
 		#print(pos_processed[i])
 	return pos_processed
 
+def percentagePronounsNouns(total_words, csv_dict):
+	# return the percentage of the text that is pronouns vs. nouns
+	nouns_count = [pos.XPOSTAG for _, pos in csv_dict.iteritems()].count("NNP")
+	print("\npercent nouns = {0:.3f}%".format((float(nouns_count)/float(total_words))*100))
+	pronouns_count = [pos.XPOSTAG for _, pos in csv_dict.iteritems()].count("PRP")
+	pronoun_percentage = float(pronouns_count)/float(total_words)
+	print("percent pronouns = {0:.3f}%".format(pronoun_percentage*100.0))
+	return pronoun_percentage
+
+def findNamedEntityAndPronoun(pos_dict):
+	# find proper nouns and full name (first/last name)
+	named_dict = {}
+	named_temp_lst = []
+	pos_type_lst = []
+	for row, pos_named in pos_dict.iteritems():
+		if "NN" in pos_named.XPOSTAG or "POS" in pos_named.XPOSTAG:
+			pos_type_lst.append((int(pos_named.SENTENCE_INDEX), int(pos_named.ID), pos_named.FORM, int(pos_named.SENTENCE_LENGTH), pos_named.XPOSTAG))
+	print(pos_type_lst)
+	total_sentence_indices = list(set([i[0] for i in pos_type_lst]))
+	sub_sentences = []
+	for index in total_sentence_indices:
+		# create sub sentences for each sentence [[0], [1])
+		sub_sentences.append([x for x in pos_type_lst if x[0] == index])
+
+	'''
+	for row, pos_named in pos_dict.iteritems():
+		#print(pos_named.SENTENCE_INDEX)
+		#print(pos_named.FORM)
+		#print(pos_named.XPOSTAG)
+		if "NN" in pos_named.XPOSTAG:
+			named_lst[int(pos_named.SENTENCE_INDEX)], int(row), pos_named.XPOSTAG, pos_named.FORM])
+		#if "PRP" in pos_named.XPOSTAG:
+		#	named_lst.append([int(pos_named.SENTENCE_INDEX), int(row), pos_named.XPOSTAG, pos_named.FORM])
+		if "POS" in pos_named.XPOSTAG:
+			named_lst.append([int(pos_named.SENTENCE_INDEX), int(row), pos_named.XPOSTAG, pos_named.FORM])
+	'''
+
+########################################################################
+## Output pos into csv
+def outputCSVconll(filename, dict_parts_speech, filednames):
+	# save conll parser and pos to csv
+	'''
+	0 - ID (index in sentence), index starts at 1
+	1 - FORM (exact word)
+	2 - LEMMA (stem of word form)
+	3 - UPOSTAG (universal pos tag)
+	4 - XPOSTAG (Language-specific part-of-speech tag)
+	5 - FEATS (List of morphological features)
+	6 - HEAD (Head of the current token, which is either a value of ID or zero (0))
+	7 - DEPREL (Universal Stanford dependency relation to the HEAD (root iff HEAD = 0))
+	8 - DEPS (List of secondary dependencies)
+	9 - MISC (other annotation)
+	'''
+	given_file = os.path.basename(os.path.splitext(filename)[0]) # return only the filename and not the extension
+	output_filename = "pos_{0}.csv".format(given_file.upper())
+
+	with open('csv_pos/{0}'.format(output_filename), 'w+') as pos_data:
+		writer = csv.DictWriter(pos_data, fieldnames=fieldnames)
+		writer.writeheader() 
+		for i in range(len(dict_parts_speech)):
+			sentence_pos_lst = dict_parts_speech[i][1]
+			for pos in sentence_pos_lst:
+				writer.writerow({'SENTENCE_INDEX': i, 
+								'FORM': pos[1],
+								'XPOSTAG': pos[4],
+								'UPOSTAG': pos[3],
+								'ID': pos[0],
+								'SENTENCE_LENGTH': len(dict_parts_speech[i][0].split()),
+								'LEMMA': pos[2],
+								'FEATS': pos[5],
+								'HEAD': pos[6],
+								'DEPREL': pos[7],
+								'DEPS':pos[8],
+								'MISC': pos[9],
+								'SENTENCE': dict_parts_speech[i][0]
+								})
+
+	print("\nCSV POS output saved as {0}".format(output_filename))
+########################################################################
+## Parse Arguments, running main
+
+if __name__ == '__main__':
+	start_time = datetime.now()
+	import argparse
+	parser = argparse.ArgumentParser(description="flag format given as: -F <filename>")
+	parser.add_argument('-F', '-filename', help="filename from Raw_Text directory")
+	args = parser.parse_args()
+
+	filename = args.F
+
+	if filename is None:
+		print("\n\tWARNING: File not given to tokenize, exiting...\n")
+		exit()
+
+	tokens_in_order = readFile(filename)
+	tokens_as_string = " ".join(tokens_in_order)
+	tokens_as_string = tokens_as_string.translate(None, "\r")
+
+	token_sentence_dict = tokenizeSentence(tokens_as_string)
+	#print(token_sentence_dict) # TODO: switch to namedTuples
+
+	# check to see if file has already been saved in csv, otherwise run script
+	given_file = os.path.basename(os.path.splitext(filename)[0]) # return only the filename and not the extension
+	output_filename = "pos_{0}.csv".format(given_file.upper())
+	csv_local_dir = "{0}/csv_pos/{1}".format(os.getcwd(), output_filename)
+
+	fieldnames = ['SENTENCE_INDEX',
+				'FORM',
+				'XPOSTAG',
+				'UPOSTAG',
+				'ID',
+				'SENTENCE_LENGTH',
+				'LEMMA',
+				'FEATS',
+				'HEAD',
+				'DEPREL',
+				'DEPS',
+				'MISC',
+				'SENTENCE'
+				]
+
+	# if file has been modified more recently than the associated csv
+	file_has_been_modified_recently = False
+	if os.path.isfile(csv_local_dir): # if file exists, then check if modified
+		file_has_been_modified_recently = os.path.getmtime("{0}/{1}".format(os.getcwd(), filename)) > os.path.getmtime(csv_local_dir)
+	# if file does not exist in the csv folder
+	if not os.path.isfile(csv_local_dir) or file_has_been_modified_recently: 
+		#print("pos needs to be calculated...")
+		dict_parts_speech = partsOfSpeech(token_sentence_dict)
+		outputCSVconll(filename, dict_parts_speech, fieldnames)
+
+	
+
+	#TODO Next: import local file to predict male/female (he/she) with a given list of names
+	#x number of sentences around to find proper noun
+	#from sklearn.externals import joblib # save model to load
+	#loaded_gender_model = joblib.load('name_preprocessing/gender_saved_model_0.853992787223.sav')
+	#test_name = ["Nemo"]
+	#print(loaded_gender_model.score(test_name))
+	#run gender tag once on the entire text, tag male/female and use for predictions
+	
+	# create named tuple from csv row
+	PosCSV = namedtuple('PosCSV', fieldnames)
+	pos_dict = {}
+	total_words = 0
+	with open(csv_local_dir, "rb") as csv_file:
+		csvreader = csv.reader(csv_file)
+		next(csvreader) # skip header
+		id_count = 0
+		for line in csvreader:
+			pos_named_tuple = PosCSV._make(line)
+			pos_dict[id_count] = pos_named_tuple
+			id_count += 1
+			if pos_named_tuple.MISC != 'punct' and pos_named_tuple.XPOSTAG != 'POS': # if row isn't puntuation or 's
+				total_words += 1
+	print(total_words)
+	named_ent_lst = findNamedEntityAndPronoun(pos_dict)
+	pronoun_percentage = percentagePronounsNouns(total_words, pos_dict) # TODO: update nouns list with only named enitites
+
+	# TODO: identify dialouge to create it as its own sentence
+
+
+	print("\nPre-processing ran for {0}".format(datetime.now() - start_time))
+
+'''
 def mostCommonPronouns(raw_text):
 	# returns a dictionary of the most common pronouns in the text with their occureance #
 	#{'it': 1291, 'him': 213, 'yourself': 16, 'his': 519, 'our': 292, 'your': 122}
@@ -122,6 +293,7 @@ def mostCommonPronouns(raw_text):
 
 	return pronoun_common
 
+
 def indexPronoun(token_dict, pronoun_dict):
 	# stores pronoun and location in sentence for each sentence
 	#{0: (['I'], [8]), 1: (['my', 'me'], [3, 21]), 2: (['I'], [5])}
@@ -139,136 +311,4 @@ def indexPronoun(token_dict, pronoun_dict):
 		index_pronoun_dict[index] = (pronoun_in_sentence, pronoun_location)
 
 	return index_pronoun_dict
-
-def findFullNames():
-	# find full names as two proper nouns next to eachother in a sentence
-	for sentence in token_sentence_dict:
-		names_proper_nouns = list(nltk.bigrams(sentence.split()))
-	pass
-
-########################################################################
-## Output pos into csv
-def outputCSVconll(filename, dict_parts_speech, pronouns_dict):
-	# save conll parser and pos to csv
-	'''
-	0 - ID (index in sentence), index starts at 1
-	1 - FORM (exact word)
-	2 - LEMMA (stem of word form)
-	3 - UPOSTAG (universal pos tag)
-	4 - XPOSTAG (Language-specific part-of-speech tag)
-	5 - FEATS (List of morphological features)
-	6 - HEAD (Head of the current token, which is either a value of ID or zero (0))
-	7 - DEPREL (Universal Stanford dependency relation to the HEAD (root iff HEAD = 0))
-	8 - DEPS (List of secondary dependencies)
-	9 - MISC (other annotation)
-	'''
-	given_file = os.path.basename(os.path.splitext(filename)[0]) # return only the filename and not the extension
-	output_filename = "pos_{0}.csv".format(given_file.upper())
-
-	with open('csv_pos/{0}'.format(output_filename), 'w+') as pos_data:
-		fieldnames = ['SENTENCE_INDEX',
-					'SENTENCE',
-					'SENTENCE_LENGTH',
-					'PRONOUN',
-					'PRONOUN_INDEX',
-					'ID',
-					'FORM',
-					'LEMMA',
-					'UPOSTAG',
-					'XPOSTAG',
-					'FEATS',
-					'HEAD',
-					'DEPREL',
-					'DEPS',
-					'MISC']
-					
-		writer = csv.DictWriter(pos_data, fieldnames=fieldnames)
-		writer.writeheader() 
-		for i in range(len(dict_parts_speech)):
-			sentence_pos_lst = dict_parts_speech[i][1]
-			for pos in sentence_pos_lst:
-				writer.writerow({'SENTENCE_INDEX': i, 
-								'SENTENCE': dict_parts_speech[i][0],
-								'SENTENCE_LENGTH': len(dict_parts_speech[i][0].split()),
-								'PRONOUN': str(pronouns_dict[i][0]).strip("[]"),
-								'PRONOUN_INDEX': str(pronouns_dict[i][1]).strip("[]"),
-								'ID': pos[0],
-								'FORM': pos[1],
-								'LEMMA': pos[2],
-								'UPOSTAG': pos[3],
-								'XPOSTAG': pos[4],
-								'FEATS': pos[5],
-								'HEAD': pos[6],
-								'DEPREL': pos[7],
-								'DEPS':pos[8],
-								'MISC': pos[9]
-								})
-
-	print("\nCSV POS output saved as {0}".format(output_filename))
-
-## save values from an existing pos from csv
-def savePOSfromExistingCSV(csv_filename):
-	# save data from existing csv
-	import pandas as pd
-	pos_csv = pd.read_csv(csv_filename)
-	print(pos_csv.head())
-	#print(pos_csv.shape)
-	#print("total features csv: {0}".format(pos_csv.shape[1]))
-	#print("total rows in csv: {0}".format(pos_csv.index))
-	#print(pos_csv[["SENTENCE_INDEX", "FORM", "UPOSTAG", "HEAD"]])
-
-	#print(pos_csv['SENTENCE_INDEX'])
-	#print(pos_csv.index)
-	#print(pos_csv.columns)
-	print("\nAverage Sentence length: {0}".format(pos_csv["SENTENCE_LENGTH"].mean()))
-	return pos_csv
-########################################################################
-## Parse Arguments, running main
-
-if __name__ == '__main__':
-	start_time = datetime.now()
-	import argparse
-	parser = argparse.ArgumentParser(description="flag format given as: -F <filename>")
-	parser.add_argument('-F', '-filename', help="filename from Raw_Text directory")
-	args = parser.parse_args()
-
-	filename = args.F
-
-	if filename is None:
-		print("\n\tWARNING: File not given to tokenize, exiting...\n")
-		exit()
-
-	tokens_in_order = readFile(filename)
-	tokens_as_string = " ".join(tokens_in_order)
-	tokens_as_string = tokens_as_string.translate(None, "\r")
-
-	# return the most common pronouns in the text (TODO: Automate)
-	most_common_pronouns_dict = mostCommonPronouns(tokens_as_string)
-	
-	token_sentence_dict = tokenizeSentence(tokens_as_string)
-	#print(token_sentence_dict) # TODO: switch to namedTuples
-
-	pronouns_dict = indexPronoun(token_sentence_dict, most_common_pronouns_dict)
-
-	# check to see if file has already been saved in csv, otherwise run script
-	given_file = os.path.basename(os.path.splitext(filename)[0]) # return only the filename and not the extension
-	output_filename = "pos_{0}.csv".format(given_file.upper())
-	csv_local_dir = "{0}/csv_pos/{1}".format(os.getcwd(), output_filename)
-	# if file has been modified more recently than the associated csv
-	file_has_been_modified_recently = os.path.getmtime("{0}/{1}".format(os.getcwd(), filename)) > os.path.getmtime(csv_local_dir)
-	# if file does not exist in the csv folder
-	if not os.path.isfile(csv_local_dir) or file_has_been_modified_recently: 
-		#print("pos needs to be calculated...")
-		dict_parts_speech = partsOfSpeech(token_sentence_dict)
-		outputCSVconll(filename, dict_parts_speech, pronouns_dict)
-
-	pos_data = savePOSfromExistingCSV(csv_local_dir)
-
-	#TODO Next: import local file to predict male/female (he/she) with a given list of names
-	#x number of sentences around to find proper noun
-	#from sklearn.externals import joblib # save model to load
-	#loaded_gender_model = joblib.load('name_preprocessing/gender_saved_model_0.853992787223.sav')
-	#test_name = ["Nemo"]
-	#print(loaded_gender_model.score(test_name))
-	#run gender tag once on the entire text, tag male/female and use for predictions
-	print("\nPre-processing ran for   {0}".format(datetime.now() - start_time))
+'''
