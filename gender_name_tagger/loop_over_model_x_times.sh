508f17f0af2a4c0bb85997c469c87f54827ff885
cschneck <cora.schneck@gmail.com>
2018-03-21 19:50:46 -0600

    update training script with probabilities

--- gender_name_tagger/loop_over_model_x_times.sh
+++ gender_name_tagger/loop_over_model_x_times.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# run model in background 100 times
+# run model in background 100 times to attempt to find more accurate model (~84%)
 
 for i in {1..100}; do
     python NN_gender_class.py
