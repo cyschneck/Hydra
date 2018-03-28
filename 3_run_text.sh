535e7728358e7837cf5cb8fdcea7b395259b0e3c
cschneck <cora.schneck@gmail.com>
2018-03-27 18:37:08 -0600

    update dir info

--- 3_run_text.sh
+++ 3_run_text.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ECHO_COMMAND=$1 
 
-cd /root/models/syntaxnet/Ishmael
+cd /root/models/syntaxnet/Hydra
 
 # return CoNLL table, output std to /dev/null
 cd /root/models/syntaxnet/ && echo $ECHO_COMMAND | ~/models/syntaxnet/syntaxnet/pos_tagger.sh  2> /dev/null
-cd Ishmael/
+cd Hydra/
 
 # return ascii tree, output std to /dev/null
 #cd /root/models/syntaxnet/ && echo $ECHO_COMMAND | ~/models/syntaxnet/syntaxnet/demo.sh 2> /dev/null
